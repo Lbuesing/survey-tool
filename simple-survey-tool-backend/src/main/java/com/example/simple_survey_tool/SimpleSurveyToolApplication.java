166f4f3c355c6c30bb17a2817cd2924d38919dae
Lukas Büsing <lukas.buesing@posteo.de>
2025-05-20 23:42:34 +0200

    LuBu cleaned up and enabled build on multiple platforms (wsl2+raspbian)

--- simple-survey-tool-backend/src/main/java/com/example/simple_survey_tool/SimpleSurveyToolApplication.java
+++ simple-survey-tool-backend/src/main/java/com/example/simple_survey_tool/SimpleSurveyToolApplication.java
@@ -2,6 +2,7 @@ package com.example.simple_survey_tool;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.SystemMetricsAutoConfiguration;
 
 @SpringBootApplication
 public class SimpleSurveyToolApplication {
